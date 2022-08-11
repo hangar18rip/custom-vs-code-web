d14bfde47c005a1c9744df0ff5498a8a3c3612a8
Olivier Delmotte <hangar18rip@users.noreply.github.com>
2022-08-11 16:42:03 +0200

    tagging

--- build-and-run.ps1
+++ build-and-run.ps1
@@ -2,11 +2,16 @@
 
 [cmdletbinding()]
 param(
-    [string] $imageName = "customvscode:latest",
+    [string] $imageName = "odestestacr.azurecr.io/customvscode",
     [switch] $publish
 )
 
 $ErrorActionPreference = "Stop"
+
+$tags = @(
+    "latest"
+    ([string]::Format("{0:yyyy}.{0:MM}.{0:dd}.{0:HHmm}", [datetime]::now))
+)
 
 $dockerId = docker ps --filter expose=3000 -q
 if($dockerId){
@@ -14,8 +19,19 @@ if($dockerId){
     docker kill $dockerId
 }
 
-docker build -t $imageName .
+$imageArgs = @()
+$tags | ForEach-Object {
+    $imageArgs += "-t ${imageName}:$_"
+}
+$imageArgs = [string]::Join(" ", $imageArgs)
+
+# docker build -t $imageName .
+Invoke-Expression "docker build $imageArgs ."
 
 if(!$publish){
     docker run -it --init -p 3000:3000 -v "$(pwd)/workspace:/home/workspace:cached" -e USERTOKEN='olivier-delmotte-at-avanade-com' $imageName
+} else {
+    $tags
+    az acr login -n odestestacr
+    docker push -a $imageName
 }
