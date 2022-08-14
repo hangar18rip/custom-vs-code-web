1e82134d3187c476e5167bef5dc037f788d210df
Olivier Delmotte <hangar18rip@users.noreply.github.com>
2022-08-14 23:08:54 +0200

    with extensions

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM ubuntu:latest
 
 RUN apt-get update && \
-    apt-get install -y wget apt-transport-https software-properties-common curl && \
+    apt-get install -y wget apt-transport-https software-properties-common curl gnome-keyring && \
     wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb && \
     dpkg -i packages-microsoft-prod.deb && \
     apt-get update && \
@@ -16,9 +16,26 @@ RUN apt install gh -y
 RUN wget -O- https://aka.ms/install-vscode-server/setup.sh | sh
 
 RUN mkdir /startup
+COPY launch.ps1 /startup
 RUN mkdir /vscode-server-datadir
 
-COPY launch.ps1 /startup
+# Creating the user and usergroup
+ARG USERNAME=vscode
+
+ARG USER_UID=1000
+ARG USER_GID=$USER_UID
+
+RUN groupadd --gid $USER_GID $USERNAME \
+    && useradd --uid $USER_UID --gid $USERNAME -m -s /bin/bash $USERNAME 
+    # \ && echo $USERNAME ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/$USERNAME 
+    # \ && chmod 0440 /etc/sudoers.d/$USERNAME
+
+RUN chown -R $USERNAME:$USERNAME /vscode-server-datadir && \
+    chmod +x /vscode-server-datadir
+RUN chown -R $USERNAME:$USERNAME /startup && \
+    chmod +x /startup
+
+USER vscode
 
 EXPOSE 8000
 
--- launch.ps1
+++ launch.ps1
@@ -2,17 +2,18 @@ $confFile = "/setup/config.json"
 if(Test-Path $confFile){
     $config = Get-Content $confFile | ConvertFrom-Json
     $config.extensions | ForEach-Object {
-        Write-Host "Installing extension $_"
+        Write-Host "Installing extension $_" -ForegroundColor Blue
         code-server serve-local --accept-server-license-terms --install-extension $_
     }
 }
 
-# /User/settings.json
-
-Write-Host "Listing exts :::" -ForegroundColor Red
+$userSettings = "/setup/config.json"
+if(Test-Path $userSettings){
+    Write-Host "Copying default user settings" -ForegroundColor Blue
+    New-Item "$($env:HOME)/.config/Code/User/" -ItemType Directory
+    Copy-Item $userSettings "$($env:HOME)/.config/Code/User/settings.json" -Force -Verbose
+}
 
 code-server serve-local --accept-server-license-terms --list-extensions --show-versions
 
-Write-Host "Hello :::" -ForegroundColor Red
-
-code-server serve-local --accept-server-license-terms --connection-token $env:USERTOKEN --host 0.0.0.0 --port 8000 --server-data-dir /vscode-server-datadir
+dbus-run-session -- sh -c "(echo 'somecredstorepass' | gnome-keyring-daemon --unlock) && code-server serve-local --accept-server-license-terms --connection-token $($env:USERTOKEN) --host 0.0.0.0 --port 8000 --server-data-dir /vscode-server-datadir"
